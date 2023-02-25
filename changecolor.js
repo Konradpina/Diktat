ca318dc778bdc1384542998342f21ee59187c8f3
Konradpina <123588475+Konradpina@users.noreply.github.com>
2023-02-25 19:27:40 +0100

    Add files via upload

--- changecolor.js
+++ changecolor.js
@@ -36,8 +36,8 @@ function colorblue(){
     //color = document.getElementById("this")
     
     for (var e=0;e < color.length;e++){
-        color[e].style.color="blue"
-        color[e].style.borderColor="blue"
+        color[e].style.color="rgb(0, 50, 255)"
+        color[e].style.borderColor="rgb(0, 50, 255)"
         color[e].style.boxShadow= "0 0 0 "
     }
 }
@@ -53,3 +53,41 @@ function coloryellow(){
     }
 }
 
+function randomcolor(){
+    color = document.getElementsByClassName("textcolor")
+    var random1= Math.floor(Math.random()*255+1)
+        var random2= Math.floor(Math.random()*255+1)
+        var random3= Math.floor(Math.random()*255+1)
+    for (var e=0;e < color.length;e++){
+        color[e].style.color=`rgb(${random1}, ${random2}, ${random3} )`
+        color[e].style.borderColor=`rgb(${random1}, ${random2}, ${random3} )`
+        color[e].style.boxShadow= "0 0 0 "
+    }
+}
+
+function partycolor(){
+    color = document.getElementsByClassName("textcolor")
+    for (var e=0;e < color.length;e++){
+        var random1= Math.floor(Math.random()*255+1)
+        var random2= Math.floor(Math.random()*255+1)
+        var random3= Math.floor(Math.random()*255+1)
+        color[e].style.color=`rgb(${random1}, ${random2}, ${random3} )`
+        color[e].style.borderColor=`rgb(${random1}, ${random2}, ${random3} )`
+        color[e].style.boxShadow= "0 0 0 "
+    }
+}
+
+// function partycolor(){
+//     color = document.getElementsByClassName("textcolor")
+//     for (var e=0;e < color.length;e++){
+//         var random1= Math.floor(Math.random()*255+1)
+//         var random2= Math.floor(Math.random()*255+1)
+//         var random3= Math.floor(Math.random()*255+1)
+//         var random5= Math.floor(Math.random()*255+1)
+//         var random4= Math.floor(Math.random()*255+1)
+//         var random6= Math.floor(Math.random()*255+1)
+//         color[e].style.color=`rgb(${random1}, ${random2}, ${random3} )`
+//         color[e].style.borderColor=`rgb(${random4}, ${random5}, ${random6} )`
+//         color[e].style.boxShadow= "0 0 0 "
+//     }
+// }
